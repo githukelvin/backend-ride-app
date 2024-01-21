0f0d85ac6078eeb5bf5c7174eda5e59fd93dcff8
githukelvin <kelvingithu019@gmail.com>
2024-01-21 12:31:14 +0300

    added notification

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use http\Client\Curl\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
 
     public function submit(Request $request){
-        $request->validate([
-
-        ]);
 //        validate phone number
+
+        $request->validate([
+            'phone'=>'required|numeric|min:10'
+        ]);
+
 //        find or create a user model
-//        send the user oOTP
-//        return back a respone
+        $user = User::firstOrCreate([
+                'phone'=>$request->phone
+        ]);
+        if(!$user){
+            return response()->json([
+                'status'=>501,
+                ['message'=>'Could not process user with that phone number']
+            ]);
+        }
+//        send the user OTP
+        $user->notify();
+
+//        return back a response
     }
 }
